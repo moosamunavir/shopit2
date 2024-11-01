6b5371368f2bb67c69770317c209c4a0bc96b417
unknown <moosakpmunavir@gmail.com>
2024-11-01 08:43:37 +0530

    Updated frontend components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/footer";
+import { Toaster } from "react-hot-toast";
+import useUserRoutes from "./components/routes/userRoutes";
+import useAdminRoutes from "./components/routes/adminRoutes";
 
 function App() {
+  const userRoutes = useUserRoutes();
+  const adminRoutes = useAdminRoutes();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+
+        <div className="container">
+          <Routes>
+            {userRoutes}
+            {adminRoutes}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
